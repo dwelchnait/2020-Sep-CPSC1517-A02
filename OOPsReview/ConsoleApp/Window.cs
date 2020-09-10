c8983b4330c4b80d9bb5a6d4091f7a39faeebcc4
Don Welch <dwelch@nait.ca>
2020-09-10 11:48:04 -0600

    Create aclass called Window

---
+++ OOPsReview/ConsoleApp/Window.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    // a class represents the defined characteristics of an item
+    // an item can be a physical thing (cellphone), concept (student),
+    //   a collection of data
+    // visual studio creates your clas without a specific access type
+    // the default type for a class is private
+    // code outside of a private class cannot use the contents of the private class???
+    // for the class to be used by an outside user (code) it must be public
+    public class Window
+    {
+        
+    }
+}
