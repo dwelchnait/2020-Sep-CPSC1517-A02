56fa89c5e7a975a51a82314f014b1a46282e3ff1
Don Welch <dwelch@nait.ca>
2020-09-17 10:59:07 -0600

    Creating instances, review of the constructor execution

--- OOPsReview/ConsoleApp/Program.cs
+++ OOPsReview/ConsoleApp/Program.cs
@@ -10,16 +10,45 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Window myInstance = new Window(); //the system calls you class constructor
+            //create an instance of the Window class using the defualt constructor
+            //the system calls your class constructor, your code NEVER calls the constructor
+            //  directly
+            //the "new" will use the indicated constructor
+            //the "new" actually makes the call to the constructor and returns the
+            //  instance of the class
 
+            //call to the Default constructor
+            Window myInstance = new Window();
+
+            //results of the constructor
+            Console.WriteLine($"Width {myInstance.Width}; Height {myInstance.Height};" +
+                $"Panes {myInstance.NumberOfPanes}; Manufacturer >{myInstance.Manufacturer}<"
+                );
+
+            //to place data within the new instance (object) of the class 
+            //  use the properties
+            //to reference a property within an instance, use the dot operator
             myInstance.Width = 1.2m;
             myInstance.Height = 1.2m;
             myInstance.NumberOfPanes = 3;
             myInstance.Manufacturer = "All-Weather Windows";
 
-            Window myGreedyInstance = new Window(1.2m, 1.2m, 3, "All-Weather Windows");
+            Console.WriteLine($"Width {myInstance.Width}; Height {myInstance.Height};" +
+               $"Panes {myInstance.NumberOfPanes}; Manufacturer >{myInstance.Manufacturer}<"
+               );
 
+            Window myGreedyInstance = new Window(1.6m, 3.3m, 3, "Fancy Windows");
 
+            Console.WriteLine($"Width {myGreedyInstance.Width}; Height {myGreedyInstance.Height};" +
+              $"Panes {myGreedyInstance.NumberOfPanes}; Manufacturer >{myGreedyInstance.Manufacturer}<"
+              );
+
+            Door theDoor = new Door(1.2m, 1.9m, "L", "wood");
+            Console.WriteLine($"Width {theDoor.Width}; Height {theDoor.Height};" +
+              $"R or L {theDoor.RightOrLeft}; Material >{theDoor.Material}<"
+              );
+
+            Console.ReadKey(); //when using the debugger your console window will remain open
         }
     }
 }
